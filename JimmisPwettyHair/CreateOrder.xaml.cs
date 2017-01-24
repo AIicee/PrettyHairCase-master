9865d683afaacd3e73a98fde0ed74a3fbbbd3909
Christine <larsen_955@hotmail.com>
2017-01-24 11:08:04 +0100

    PHGroupWPF

--- JimmisPwettyHair/CreateOrder.xaml.cs
+++ JimmisPwettyHair/CreateOrder.xaml.cs
@@ -23,5 +23,10 @@ namespace JimmisPwettyHair
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
