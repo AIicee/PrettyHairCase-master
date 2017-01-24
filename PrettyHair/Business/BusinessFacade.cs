3692f7e38f63a7759b858f10272b2d0fac28fa82
Christine <Christine@LAPTOP-8SC8G3GB>
2017-01-24 10:26:57 +0100

    Add project files.

---
+++ PrettyHair/Business/BusinessFacade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DatabaseLayer;
+using DomainLayer;
+
+namespace PrettyHair
+{
+    public class BusinessFacade
+    {
+        
+        private static volatile BusinessFacade instance;
+
+        private static object synchronizationRoot = new Object();
+
+        public static BusinessFacade Instance
+        {
+            get
+            {
+                lock (synchronizationRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new BusinessFacade();
+                    }
+                }
+                
+                return instance;
+            }
+        }
+
+        private BusinessFacade()
+        {
+            DatabaseFacade dbf = DatabaseFacade.Instance;
+        }
+
+        public void SaveCustomer(string lastName, string firstName, string address, string phoneNumber)
+        {
+            Customer newCust = new Customer(lastName, firstName, address, phoneNumber);
+            DatabaseFacade.Instance.NewCustomer(newCust);
+        }
+        public List<Customer> ShowCustomer()
+        {
+            List<Customer> custList = new List<Customer>();
+            custList.AddRange(DatabaseFacade.Instance.ShowCustomer());
+            
+            return custList;
+        }
+    }
+}
